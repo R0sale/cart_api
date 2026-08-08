2c96d84166cf46ded87bc61bf52132a3f5ced7fd
R0sale <kvusov@bk.ru>
2026-08-08 17:14:21 +0300

    Created a handler and configured the db connection and server multiplexer

--- cmd/main.go
+++ cmd/main.go
@@ -2,20 +2,11 @@ package main
 
 import (
 	"cart_api/internal/config"
-	"database/sql"
-	"fmt"
+	"cart_api/internal/db"
+	"cart_api/internal/handlers"
 	"log"
-	"net/http"
 	"os"
 )
-
-type App struct {
-	DB *sql.DB
-}
-
-func (app *App) Handle(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hi there we are done")
-}
 
 func main() {
 	configPath, ok := os.LookupEnv("CONFIG_PATH")
@@ -28,27 +19,12 @@ func main() {
 		log.Fatalf("%v", err)
 	}
 
-	dsn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", cfg.Database.Host, cfg.Database.Port, cfg.Database.User, cfg.Database.Password, cfg.Database.DbName)
-
-	db, err := sql.Open("postgres", dsn)
+	db, err := db.ConfigureDb(*cfg)
 	if err != nil {
-		log.Fatalf("couldn't open database: %v", err)
+		log.Fatalf("problems with db: %v", err)
 	}
 
 	defer db.Close()
 
-	if err := db.Ping(); err != nil {
-		log.Fatalf("couldn't ping database: %v", err)
-	}
-
-	myApp := &App{
-		DB: db,
-	}
-
-	mux := http.NewServeMux()
-
-	mux.HandleFunc("/", myApp.Handle)
-
-	serverAddress := fmt.Sprintf(":%d", cfg.Server.Port)
-	http.ListenAndServe(serverAddress, mux)
+	handlers.ConfigureServer(*cfg)
 }
